ccf6f56cba929cfb89c63042d6655282314f7f3e
Eric Yuzo <eyuzo@devsv.com.br>
2017-10-10 17:03:53 -0300

    using exit code (returns the number of errors)

--- setup.sh
+++ setup.sh
@@ -5,13 +5,12 @@ config_dotfiles() {
     for f in "$@"
     do
         echo -n "Creating file ~/.$f ...   "
-        if ! cmp $CONFIGDIR/$f ~/.$f > /dev/null 2>&1
-        then
-            if cat $CONFIGDIR/$f > ~/.$f 2> /dev/null
-            then
+        if ! cmp $CONFIGDIR/$f ~/.$f > /dev/null 2>&1 ; then
+            if cat $CONFIGDIR/$f > ~/.$f 2> /dev/null ; then
                 echo "Done"
             else
                 echo "Fail"
+                EXITCODE=$((EXITCODE + 1))
             fi
         else
             echo "Nothing to do"
@@ -22,13 +21,11 @@ config_dotfiles() {
 # download file from $1 to $2 directory
 download() {
     echo -n "Downloading $(basename $1) ...   "
-    if wget -qcP $2 $1
-    then
+    if wget -qcP $2 $1 ; then
         echo "Done"
-        return 0
     else
         echo "Fail"
-        return 1
+        EXITCODE=$((EXITCODE + 1))
     fi
 }
 
@@ -36,14 +33,18 @@ download() {
 # variables {
 BASEDIR=$(dirname $0)
 CONFIGDIR="$BASEDIR/config"
+EXITCODE=0
+PROGRAM=$(basename $0)
+
 DOT_FILES=(bash_aliases tmux.conf vimrc gitignore_global gitconfig)
 NEL_DIR=~/.vim/colors
 NEL_URL=https://raw.githubusercontent.com/EricYuzo/nel/master/colors/nel.vim
-PROGRAM=$(basename $0)
 # }
 
 
 # main {{{
 download $NEL_URL $NEL_DIR
 config_dotfiles "${DOT_FILES[@]}"
+
+exit $EXITCODE
 # }}}
